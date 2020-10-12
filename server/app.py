56678c581697ccb4f9830442087722729219b7b4
Roberto Nunes <roberto@krooze.com.br>
2020-10-12 10:23:25 -0300

    adiciona um visualizador em flask

---
+++ server/app.py
@@ -0,0 +1,24 @@
+from banco.partida import Partida
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+partida = Partida(4, 20)
+partida.iniciar()
+
+@app.route('/')
+def index():
+    ganhador = None
+    reset = request.args.get('r')
+    if reset:
+        global partida
+        partida = Partida(4, 20)
+        partida.iniciar()
+
+    update = request.args.get('u')
+    if update:
+        if not partida.terminou():
+            partida.jogar_rodada()
+            
+    ganhador = partida.ganhador()
+    return render_template('index.html', ganhador=ganhador, partida=partida)
